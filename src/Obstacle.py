ac3c8578ae8b3ce2915ef6fcb907b1595c939b2e
DAO QUANG <daoqm@rose-hulman.edu>
2023-11-01 08:17:03 -0400

    done

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -1,10 +1,15 @@
 import pygame
+
+
 class Obstacle:
-    def __init__(self, x, y, width, height, image):
+    def __init__(self, screen, image):
         self.x = 200
         self.y = 250
         self.width = 10
         self.height = 20
         self.image = pygame.image.load("../media/pumpkin_1.png")
+        self.screen = screen
+
     def draw(self):
-        
+        self.screen.blit(self.image, (self.x, self.y))
+
