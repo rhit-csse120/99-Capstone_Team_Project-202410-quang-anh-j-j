298387f8d7181e828b6f8cfe63b36c91c0576e12
DAO QUANG <daoqm@rose-hulman.edu>
2023-11-01 08:31:17 -0400

    test the hitbox of the obstacle with the tank

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 
@@ -6,8 +8,15 @@ class Obstacle:
         self.x = 200
         self.y = 250
         self.image = pygame.image.load("../media/pumpkin_1.png")
+        # self.image_1 = pygame.image.load("../media/pumpkin_2.png")
+        # self.image_2 = pygame.image.load("../media/ghost.png")
         self.image_scale = pygame.transform.scale(self.image, (100, 100))
+        # self.image_scale1 = pygame.transform.scale(self.image_1, (100, 100))
+        # self.image_scale2 = pygame.transform.scale(self.image_2, (100, 100))
+        # self.list = [self.image_scale, self.image_scale1, self.image_scale2]
+        # self.x = random.ra
         self.screen = screen
+        self.hit_box = pygame.Rect(self.x, self.y, 100, 100)
 
     def draw(self):
         self.screen.blit(self.image_scale, (self.x, self.y))
