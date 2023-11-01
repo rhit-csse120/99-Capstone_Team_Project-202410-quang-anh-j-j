8930d36a20d89d6b75d0e46a8bc6edc23829c233
DAO QUANG <daoqm@rose-hulman.edu>
2023-10-31 22:35:41 -0400

    done

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -1,2 +1,10 @@
+import pygame
 class Obstacle:
-    def __init__(self):
+    def __init__(self, x, y, width, height, image):
+        self.x = 200
+        self.y = 250
+        self.width = 10
+        self.height = 20
+        self.image = pygame.image.load("../media/pumpkin_1.png")
+    def draw(self):
+        
