83d3b28819e9ac56c069fe54feace185f0cd36ee
Anh Ngo <145743526+Anh-Q-Ngo@users.noreply.github.com>
2023-11-01 08:27:31 -0400

    Refined Bullet

--- src/Bullet.py
+++ src/Bullet.py
@@ -8,18 +8,23 @@ class Bullet:
         self.y = y
         self.h_speed = 10
         self.v_speed = 0
+        self.height = 6
+        self.width = 4
         self.hit_by_tank = False
         self.hit_by_obstacles = False
+        self.has_exploded = False
+        self.color = "magenta"
 
     def draw(self):
-        pygame.draw.line(self.screen, "magenta",
+        pygame.draw.line(self.screen, self.color,
                          (self.x, self.y),
-                         (self.x + 5, self.y))
+                         (self.x + self.height, self.y),
+                         self.width)
 
     def move(self):
         self.x = self.x + self.h_speed
         self.y = self.y + self.v_speed
 
     def explode(self):
-        pass
+        self.has_exploded = True
 
