47691a2d5fe03a88669656f45d1034c16a5fbfb9
Anh Ngo <145743526+Anh-Q-Ngo@users.noreply.github.com>
2023-11-03 10:38:56 -0400

    Update Bullet.py

--- src/Bullet.py
+++ src/Bullet.py
@@ -28,33 +28,3 @@ class Bullet:
     def explode(self):
         self.has_exploded = True
 
-import pygame
-
-
-class Bullet:
-    def __init__(self, screen: pygame.Surface, x, y):
-        self.screen = screen
-        self.x = x
-        self.y = y
-        self.h_speed = 10
-        self.v_speed = 0
-        self.height = 6
-        self.width = 4
-        self.hit_by_tank = False
-        self.hit_by_obstacles = False
-        self.has_exploded = False
-        self.color = "magenta"
-
-    def draw(self):
-        pygame.draw.line(self.screen, self.color,
-                         (self.x, self.y),
-                         (self.x + self.height, self.y),
-                         self.width)
-
-    def move(self):
-        self.x = self.x + self.h_speed
-        self.y = self.y + self.v_speed
-
-    def explode(self):
-        self.has_exploded = True
-
